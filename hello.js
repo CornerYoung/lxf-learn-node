25e9d157981bf1ede098f07be10f433300ea4b7a
ygn <947498533@qq.com>
2018-04-09 13:52:47 +0800

    模块化

--- hello.js
+++ hello.js
@@ -1,2 +1,9 @@
 'use strict';
-console.log("hello world!")
+
+var s = 'Hello';
+
+function greet(name) {
+    console.log(s + ', ' + name + '!');
+}
+
+module.exports = greet;
---
+++ main.js
@@ -0,0 +1,6 @@
+'use strict'
+var greet = require('./hello');
+
+var s = "corner";
+
+greet(s);
